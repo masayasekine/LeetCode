eadcf082cc357c2b5086bd7c95af6357d2f68971
masayasekine <negirider250@gmail.com>
2022-12-13 21:39:09 +0900

    1_approved

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, num_a in enumerate(nums):
-            for j, num_b in enumerate(nums):
-                if i == j:
-                    continue
-                if num_a + num_b == target:
-                    return [i, j]
+        hashmap = {}
+        for i, num in enumerate(nums):
+            hashmap[num] = i
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
         return []
